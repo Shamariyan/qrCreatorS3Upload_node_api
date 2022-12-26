7d9e041ae459da1a07a6b679730d310f63afebcc
Shamariyan <sharanbalakrish97@gmail.com>
2022-12-26 20:28:07 +0530

    full code of lambda updated

--- index.js
+++ index.js
@@ -10,23 +10,14 @@ const app = express();
 
 app.use(express.json());
 
-// const S3 = new AWS.S3({
-// 	accessKeyId: 'AKIA2HXTNWNM34RUS2PR',
-// 	secretAccessKey: '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',
-// });
 const S3 = new AWS.S3({
 	accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
 	secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
 });
-const varia = process.env.VAR_VAR;
-const nm = process.env.BUCKET_NAME;
 
-app.get('/', (req, res) => {
-	res.send(`API ${nm} running ${varia} on ${PORT}`);
-});
+app.get('/', (req, res) => res.send(`API running on ${PORT}`));
 
 app.post('/', async (req, res, next) => {
-	//mongo db connection string
 	const uri = process.env.MONGO_URI;
 
 	const client = new MongoClient(uri, {
@@ -34,10 +25,10 @@ app.post('/', async (req, res, next) => {
 		useUnifiedTopology: true,
 		serverApi: ServerApiVersion.v1,
 	});
+
 	const { orderId } = req.body;
 
 	const rouletteUrl = `https://roulette-api.qr8code.com/roulette/${orderId}`;
-
 	const background = fs.readFileSync('qrbg.jpg');
 
 	const buffer = await new AwesomeQR({
@@ -51,7 +42,8 @@ app.post('/', async (req, res, next) => {
 	const baseImage = await axios.get(baseImageUrl, {
 		responseType: 'arraybuffer',
 	});
-	//merger images
+
+	// Merging two images using sharp
 	sharp(baseImage.data)
 		.composite([
 			{
@@ -68,7 +60,6 @@ app.post('/', async (req, res, next) => {
 				Key: `${orderId}.png`,
 				Body: updatedBuffer,
 			};
-			fs.writeFileSync('assss.png', updatedBuffer);
 			S3.upload(params, async (err, data) => {
 				if (err) {
 					return res.status(500).json({
@@ -76,53 +67,53 @@ app.post('/', async (req, res, next) => {
 					});
 				}
 				res.send('uploaded to S3');
-				// res.send(updatedBuffer);
-				// 	let cloudFrontUrl = `https://d3rwni9jzymvdb.cloudfront.net/${orderId}.png`;
-				// 	client.connect();
+				let cloudFrontUrl = `https://d3rwni9jzymvdb.cloudfront.net/${orderId}.png`;
+				client.connect();
 
-				// 	const collection = client.db('qr8Code').collection('qr8_Collection');
-				// 	// Find the order details
-				// 	const response = await collection
-				// 		.find({
-				// 			_id: ObjectId(orderId),
-				// 		})
-				// 		.toArray();
-				// 	const { orderEmail, customerName } = response[0];
-				// 	const updateResponse = await collection.findOneAndUpdate(
-				// 		{
-				// 			_id: ObjectId(orderId),
-				// 		},
+				const collection = client.db('qr8Code').collection('qr8_Collection');
+				// Find the order details
+				const response = await collection
+					.find({
+						_id: ObjectId(orderId),
+					})
+					.toArray();
+				const { orderEmail, customerName } = response[0];
+				const updateResponse = await collection.findOneAndUpdate(
+					{
+						_id: ObjectId(orderId),
+					},
 
-				// 		{
-				// 			$set: {
-				// 				imageUrl: cloudFrontUrl,
-				// 				rouletteUrl,
-				// 			},
-				// 		}
-				// 	);
-				// 	//Calling email lambda function
-				// 	const body = {
-				// 		email: orderEmail,
-				// 		awsUrl: cloudFrontUrl,
-				// 		name: customerName,
-				// 		orderId: orderId,
-				// 		rouletteUrl: rouletteUrl,
-				// 	};
-				// 	const Config = {
-				// 		headers: {
-				// 			'Content-Type': 'application/json',
-				// 		},
-				// 	};
-				// 	try {
-				// 		const response = await axios.post(
-				// 			'https://1ncrakw0d4.execute-api.us-west-1.amazonaws.com',
-				// 			body,
-				// 			Config
-				// 		);
-				// 		return res.status(200).send(response.data);
-				// 	} catch (error) {
-				// 		return res.status(500).send(error);
-				// 	}
+					{
+						$set: {
+							imageUrl: cloudFrontUrl,
+							rouletteUrl,
+						},
+					}
+				);
+				//Calling email lambda function
+				const body = {
+					email: orderEmail,
+					awsUrl: cloudFrontUrl,
+					name: customerName,
+					orderId: orderId,
+					rouletteUrl: rouletteUrl,
+				};
+				const Config = {
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				};
+				try {
+					// Calling email service lambda
+					const response = await axios.post(
+						'https://1ncrakw0d4.execute-api.us-west-1.amazonaws.com',
+						body,
+						Config
+					);
+					return res.status(200).send(response.data);
+				} catch (error) {
+					return res.status(500).send(error);
+				}
 			});
 		})
 		.catch((err) => {
@@ -131,8 +122,5 @@ app.post('/', async (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const variab = process.env.VAR_VAR;
 
-app.listen(PORT, () =>
-	console.log(`SERVER (${variab}) Started runnning on ${PORT}`)
-);
+app.listen(PORT, () => console.log(`SERVER Started running on ${PORT}`));
