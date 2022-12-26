55aff864dabf268312ac48dd37d4c682ae2e1162
Shamariyan <sharanbalakrish97@gmail.com>
2022-12-26 09:18:16 +0530

    minor

--- index.js
+++ index.js
@@ -127,6 +127,6 @@ app.post('/', async (req, res, next) => {
 		});
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`SERVER Started runnning on ${PORT}`));
